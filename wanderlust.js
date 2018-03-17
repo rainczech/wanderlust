61ee118a694f47673c367490b59e11294542a9ed
Grace Ho <rainczech@gmail.com>
2018-03-17 13:19:10 -0500

    firebase added

---
+++ wanderlust.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCSGFZALM3AFGAO6l7rYs0FQxo9skFpoTM",
+    authDomain: "wanderlust-17647.firebaseapp.com",
+    databaseURL: "https://wanderlust-17647.firebaseio.com",
+    projectId: "wanderlust-17647",
+    storageBucket: "",
+    messagingSenderId: "285359026722"
+  };
+  firebase.initializeApp(config);
