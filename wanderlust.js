f719bab727d4c2e5a975df0624ae71d7783ac465
Mike Smith <mike.smith1@earthlink.net>
2018-03-21 19:41:49 -0500

    mike's code yall

--- wanderlust.js
+++ wanderlust.js
@@ -1,19 +1,24 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
 
 
 $(document).ready(function() {
-    $('#search').click(function() {
-      let city = $('#location').val();
-      $('#location').val("");
+    $('#searchForm').on("submit", function(event) {
+      event.preventDefault();
+      let city = $('#search').val();
+      $('#search').val("");
       $.ajax({
         url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=7fbd1c73cafb287415d9c63a48da591a`,
         type: 'GET',
         data: {
           format: 'json'
+        
         },
+        
         success: function(response) {
-          $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-          $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+          $('#showCity').text(`Current Weather in ${response.name}:`);
+          $('.showTemp').html(`${response.main.temp}&deg;F`);
+          $('.showCondition').text(`${response.weather[0].description}`);
+          $('#conditionIcon').attr("src", `http://openweathermap.org/img/w/${response.weather[0].icon}.png`);
           console.log(response);
         },
         error: function() {
@@ -24,29 +29,30 @@ $(document).ready(function() {
 
     });
   });
-
+  
   console.log("connected");
-=======
-var config = {
-    apiKey: "AIzaSyCSGFZALM3AFGAO6l7rYs0FQxo9skFpoTM",
-    authDomain: "wanderlust-17647.firebaseapp.com",
-    databaseURL: "https://wanderlust-17647.firebaseio.com",
-    projectId: "wanderlust-17647",
-    storageBucket: "",
-    messagingSenderId: "285359026722"
-  };
-  firebase.initializeApp(config);
+// =======
+// var config = {
+//     apiKey: "AIzaSyCSGFZALM3AFGAO6l7rYs0FQxo9skFpoTM",
+//     authDomain: "wanderlust-17647.firebaseapp.com",
+//     databaseURL: "https://wanderlust-17647.firebaseio.com",
+//     projectId: "wanderlust-17647",
+//     storageBucket: "",
+//     messagingSenderId: "285359026722"
+//   };
+//   firebase.initializeApp(config);
 
-var database = firebase.database();
+// var database = firebase.database();
 
-$(".user-input").on("keypress", function(e) {
-    if(e.keycode == 13) {
-        var input = $(".user-input").val().trim();
-        database.ref.push({input: input});
-
-
+// $(".user-input").on("keypress", function(e) {
+//     if(e.keycode == 13) {
+//         var input = $(".user-input").val().trim();
+//         database.ref.push({input: input});
 
 
-    }
+
+
+    // }
    
->>>>>>> ee7506f21837c15ab5326478142bbc0a8842b877
+// >>>>>>> ee7506f21837c15ab5326478142bbc0a8842b877
+  // })
